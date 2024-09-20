5fc0d36a0469874a51eaf1dd269eb4648b0f1a84
George Kirkman <gkirkman@rollingrobots.com>
2024-09-19 18:15:53 -0700

    config

--- PitMonitor/src/robot-config.cpp
+++ PitMonitor/src/robot-config.cpp
@@ -9,12 +9,12 @@ brain  Brain;
 
 // VEXcode device constructors
 controller Controller1 = controller(primary);
-motor LeftFront = motor(PORT2, ratio18_1, true);
-motor LeftBack = motor(PORT4, ratio18_1, false);
-motor RightFront = motor(PORT1, ratio18_1, false);
-motor RightBack = motor(PORT3, ratio18_1, true);
-motor LeftMid = motor(PORT5, ratio18_1, false);
-motor RightMid = motor(PORT7, ratio18_1, true);
+motor LeftFront = motor(PORT16, ratio18_1, true);
+motor LeftBack = motor(PORT12, ratio18_1, false);
+motor RightFront = motor(PORT3, ratio18_1, false);
+motor RightBack = motor(PORT9, ratio18_1, true);
+motor LeftMid = motor(PORT7, ratio18_1, false);
+motor RightMid = motor(PORT10, ratio18_1, true);
 
 // VEXcode generated functions
 // define variable for remote controller enable/disable
