6fc8a39b38e8191b771e94ce62216aeb0406ed26
George Kirkman <gkirkman@rollingrobots.com>
2025-07-23 13:10:14 -0700

    battle Claw bots
    c

---
+++ battleRobots/src/main.cpp
@@ -0,0 +1,104 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       georgekirkman                                             */
+/*    Created:      7/23/2025, 12:55:25 PM                                    */
+/*    Description:  V5 brain 393 motor clawbots                                               */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of competition
+competition Competition;
+
+// define your global instances of motors and other devices here
+brain Brain;
+controller Controller1;
+motor29 LM= motor29(Brain.ThreeWirePort.A, false);
+motor29 RM= motor29(Brain.ThreeWirePort.B, true);
+motor29 Claw = motor29(Brain.ThreeWirePort.C);
+
+
+void drive(int lspeed, int rspeed, int wt){
+  LM.spin(forward, lspeed, pct);
+  RM.spin(forward, rspeed, pct);
+  wait(wt, msec);
+}
+
+void claw(int speed){
+  Claw.spin(forward, speed, pct);
+}
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+void pre_auton(void) {
+
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void autonomous(void) {
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void usercontrol(void) {
+  // User control code here, inside the loop
+  while (1) {
+
+    drive(Controller1.Axis3.position(),Controller1.Axis2.position(), 10);
+
+    if(Controller1.ButtonR1.pressing()) claw(100);
+    else if(Controller1.ButtonR2.pressing()) claw(-100);
+    else claw(0);
+    
+  }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main() {
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+  }
+}
